f4409399dbaad44964c5f7bc7858479a045c2fab
Aleksandar Jovanov <jovanov33@gmail.com>
2019-05-31 10:06:27 +0200

    Base http service added.

---
+++ Project/Frontend/PublicTransport/src/app/services/base-http.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BaseHttpService<T> {
+
+  baseUrl = "http://localhost:52295";
+  specifiedUrl = "";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<T[]>{
+    return this.httpClient.get<T[]>(this.baseUrl + this.specifiedUrl);
+  }
+
+  getById(id: number): Observable<T>{
+    return this.httpClient.get<T>(this.baseUrl + this.specifiedUrl + `/${id}`);
+  }
+
+  put(id: number, inputData: T){
+    return this.httpClient.put(this.baseUrl + this.specifiedUrl + `/${id}`, inputData);
+  }
+
+  post(inputData: any): Observable<T>{
+    return this.httpClient.post<T>(this.baseUrl + this.specifiedUrl, inputData);
+  }
+
+  delete(id: number): Observable<T>{
+    return this.httpClient.delete(this.baseUrl + this.specifiedUrl + `/${id}`);
+  }
+}
