4604783ae9ef12b286eb5ea84c4a6ac7ab8661ba
Nikola Karakli? <nikolakaraklic@gmail.com>
2019-06-03 11:36:39 +0200

    [Frontend] Added initial components (except for login, it already existed).

--- Project/Frontend/PublicTransport/src/app/app.module.ts
+++ Project/Frontend/PublicTransport/src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from 'src/app/interceptors/token.interceptor';
 import { LoginComponent } from './components/login/login.component';
 import { AuthHttpService } from './services/auth-http.service';
 import { PLTTService } from './services/price-list-ticket-type-http.service';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentComponent } from './components/content/content.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PassengerComponent } from './components/passenger/passenger.component';
+import { ControllerComponent } from './components/controller/controller.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContentComponent,
+    RegisterComponent,
+    PassengerComponent,
+    ControllerComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
