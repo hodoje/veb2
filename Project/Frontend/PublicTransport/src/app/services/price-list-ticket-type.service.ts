6894f888796414c3422d658f6189c0eb6e867e59
Aleksandar Jovanov <jovanov33@gmail.com>
2019-06-03 13:58:01 +0200

    [Frontend] PLTTService changed to a different file.

---
+++ Project/Frontend/PublicTransport/src/app/services/price-list-ticket-type.service.ts
@@ -0,0 +1,6 @@
+import { BaseHttpService } from './base-http.service';
+import { PriceListTicketType } from '../models/pricelisttickettype.model';
+
+export class PLTTService extends BaseHttpService<PriceListTicketType>{
+    specifiedUrl = "api/TicketTypePricelists"
+}
