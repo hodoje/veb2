82c5d8ddf8803ce0e07a514ee2772d3562a4ade4
hodoje <nikolakaraklic@gmail.com>
2019-06-08 03:21:35 +0200

    Finished admin/pricelist component.

--- Project/Frontend/PublicTransport/src/app/components/pricelist-modification/pricelist-modification.component.ts
+++ Project/Frontend/PublicTransport/src/app/components/pricelist-modification/pricelist-modification.component.ts
@@ -1,4 +1,7 @@
+import { Pricelist } from './../../models/pricelist.model';
+import { PricelistHttpService } from './../../services/pricelist-http.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-pricelist-modification',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricelistModificationComponent implements OnInit {
 
-  constructor() { }
+  activePricelist: Pricelist
+  currentDate: Date
+  minDate = new Date().toISOString().slice(0,10)
+  hourly: number
+  daily: number
+  monthly: number
+  yearly: number
+
+  constructor(private pricelistService: PricelistHttpService) {
+    this.activePricelist = new Pricelist();
+  }
 
   ngOnInit() {
+    this.pricelistService.getActivePricelist().subscribe(
+      (data: Pricelist) => {
+        this.activePricelist = data;
+        this.currentDate = this.activePricelist.fromDate;
+        this.hourly = this.activePricelist.hourly;
+        this.daily = this.activePricelist.daily;
+        this.monthly = this.activePricelist.monthly;
+        this.yearly = this.activePricelist.yearly;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
+
+  updatePricelist(newPricelist: Pricelist, form: NgForm){
+    newPricelist.fromDate = this.currentDate;
+    this.pricelistService.post(newPricelist).subscribe(
+      (data) => {
+        form.reset({
+          hourly: "",
+          daily: "",
+          monthly: "",
+          yearly: ""
+        });
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  parseDate(dateString: string): Date {
+    if (dateString) {
+        return new Date(dateString);
+    }
+    return null;
+}
 
 }
