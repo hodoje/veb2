bf99d978f65095051729c90cb3079e484ec33434
fgbjoki <jovanov33@gmail.com>
2019-06-12 16:17:27 +0200

    [Backend] File relocation and JsonProperty defined for Hub camelCase.

--- Project/Backend/WebApp/WebApp/Models/Dtos/ApplicationUserDto.cs
+++ Project/Backend/WebApp/WebApp/Models/Dtos/ApplicationUserDto.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using WebApp.Models.Dtos;
 
 namespace WebApp.Models.DomainModels
 {
     public class ApplicationUserDto
-    {        
+    {
+        [JsonProperty("email")]
         public string Email { get; set; }
-        public string Name { get; set; }        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("lastname")]
         public string Lastname { get; set; }
+        [JsonProperty("birthday")]
         public DateTime Birthday { get; set; }
+        [JsonProperty("address")]
         public string Address { get; set; }
+        [JsonProperty("isSuccessfullyRegistered")]
         public bool IsSuccessfullyRegistered { get; set; }
+        [JsonProperty("documentImage")]
         public string DocumentImage { get; set; }
+        [JsonProperty("userType")]
         public UserTypeDto UserType { get; set; }
     }
 }
--- Project/Backend/WebApp/WebApp/Models/Dtos/UserTypeDto.cs
+++ Project/Backend/WebApp/WebApp/Models/Dtos/UserTypeDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ namespace WebApp.Models.Dtos
 {
     public class UserTypeDto
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
     }
 }
